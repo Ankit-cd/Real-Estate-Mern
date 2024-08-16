b9e8aefb34c72d9533d23d4a9b57e01849ceac8a
Ankit_cd <ankitmehta4526@gmail.com>
2024-08-16 09:53:03 +0530

    Connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
 
 const app=express()
+
+mongoose.connect(process.env.DATABASE_URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+}).then(()=>(console.log('DB CONNECTED SUCCESSFULLY')))
+.catch((error)=>{
+    console.log('DB CONECTION FAILED');
+    console.log(error)
+    process.exit(1);
+})
 
 
 app.listen(3000,()=>{
