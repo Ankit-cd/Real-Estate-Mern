3f3db2e74e29f6a2f3a569c4ea3a7c49ac403db7
Ankit_cd <ankitmehta4526@gmail.com>
2024-08-21 02:19:27 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRouter from './routes/userRoute.js'
 dotenv.config();
 
 
@@ -19,7 +20,9 @@ mongoose.connect(process.env.DATABASE_URL,{
 })
 
 
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000');
+app.listen(4000,()=>{
+    console.log('Server is running on port 4000');
 }
 );
+
+app.use("/api/users",userRouter)
