a1d7cea9f2c33bacb2347e4595a285d1891fdd40
Ankit_cd <ankitmehta4526@gmail.com>
2024-10-21 12:51:30 +0530

    Create updatelisting api route

--- api/controllers/listingController.js
+++ api/controllers/listingController.js
@@ -12,7 +12,6 @@ export const createListing = async (req,res,next) =>{
         next(error);
     }
 
-
 }
 
 export const deleteListing = async (req,res,next)=>{
@@ -33,4 +32,26 @@ export const deleteListing = async (req,res,next)=>{
         next(error);
     }
 
+}
+
+export const updateListing = async (req,res,next) =>{
+    const listing = await Listing.findById(req.params.id);
+    if(!listing){
+        return next(errorHandler(404,'Listing not found'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401,'you can only update your own listings'))
+    }
+
+    try{
+        const updatedListing = await Listing.findByIdAndUpdate(req.params.id,req.body,
+            {new:true}
+        );
+        res.status(200).json(updatedListing);
+
+    }catch(error){
+        next(error);
+    }
+
+    
 }
