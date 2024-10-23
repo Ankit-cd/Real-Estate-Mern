1f067d9116996dfd92ac893f639f093b9a9f9b97
Ankit_cd <ankitmehta4526@gmail.com>
2024-10-23 19:47:20 +0530

    Add pre Installation

--- api/index.js
+++ api/index.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv';
 import userRouter from './routes/userRoute.js'
 import authRouter from './routes/authRoute.js'
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listingRoute.js'
+import listingRouter from './routes/listingRoute.js';
+import path from 'path';
 dotenv.config();
 
 
@@ -15,6 +16,12 @@ app.use(express.json());
 
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname,`/client/dist`)));
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+})
+
 mongoose.connect(process.env.DATABASE_URL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
@@ -23,7 +30,9 @@ mongoose.connect(process.env.DATABASE_URL,{
     console.log('DB CONECTION FAILED');
     console.log(error)
     process.exit(1);
-})
+});
+
+const __dirname = path.resolve();
 
 
 app.listen(3000,()=>{
